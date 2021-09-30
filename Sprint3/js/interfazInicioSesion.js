3d8c369122b163bb18cc9097979387655e44ebed
Wilson Mario <acordesparadios@gmail.com>
2021-09-30 17:41:39 -0500

    Creación archivos js de inicio de sesión

---
+++ Sprint3/js/interfazInicioSesion.js
@@ -0,0 +1,27 @@
+document.querySelector('#btnIngresar').addEventListener('click',
+iniciarsesion);
+function iniciarsesion(){
+    var snombreUsuario='';
+    var sclave='';
+    bacceso = false;
+    snombreUsuario = document.querySelector('#txtuserName').value;
+    sclave = document.querySelector('#txtClave').value;
+
+    bacceso = validarCredenciales(snombreUsuario,sclave);
+    // console.log(bacceso);
+        if (bacceso == true){
+            ingresar();
+        }
+        if (bacceso == false){
+            error();
+        }
+    }
+
+function ingresar(){
+    window.location.href = 'listadoreservas.html'
+}
+
+function error(){
+    alert("Datos Invalidos");
+}
+
